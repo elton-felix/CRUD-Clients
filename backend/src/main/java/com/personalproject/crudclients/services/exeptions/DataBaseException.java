57113e8fd2eb1a9f3dfcc9df4e3eef1892bcbae2
EltonFelix <eltomfelix@gmail.com>
2022-03-16 18:47:37 -0300

    exception correction

--- backend/src/main/java/com/personalproject/crudclients/services/exeptions/DataBaseException.java
+++ backend/src/main/java/com/personalproject/crudclients/services/exeptions/DataBaseException.java
@@ -2,9 +2,6 @@ package com.personalproject.crudclients.services.exeptions;
 
 public class DatabaseException extends RuntimeException{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public DatabaseException(String message) {
