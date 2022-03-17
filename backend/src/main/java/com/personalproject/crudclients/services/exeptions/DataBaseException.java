cc8339db95ef3427ecc4375225e06303207bec55
EltonFelix <eltomfelix@gmail.com>
2022-03-16 21:46:08 -0300

    DatabaseException fix

--- backend/src/main/java/com/personalproject/crudclients/services/exeptions/DataBaseException.java
+++ backend/src/main/java/com/personalproject/crudclients/services/exeptions/DataBaseException.java
@@ -4,6 +4,7 @@ public class DatabaseException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
 	
+	//
 	public DatabaseException(String message) {
 		super(message);
 	}
